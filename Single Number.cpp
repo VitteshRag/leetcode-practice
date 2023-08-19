1db6238b29b18385451e07d8415a328ba810bcc6
Vittesh Raghavan <vitteshraghavan@gmail.com>
2023-08-19 17:03:45 +0530

    Used XOR for Single Number

--- Single Number.cpp
+++ Single Number.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        int xorr=0;
         for(int i=0;i<nums.size();i++)
         {
-            int count=0;
-            for(int j=0;j<nums.size();j++)
-            {
-                if(nums[i]==nums[j])
-                count++;
-            }
-            if(count==1)
-            {
-                return nums[i];
-            }
+            xorr=xorr^nums[i];
         }
-            return 0;
+        return xorr;
     }
 };
